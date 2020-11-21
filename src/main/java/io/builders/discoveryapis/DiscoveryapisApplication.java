1d67641d58599a6f096483a685af4d28256578e9
simonini <lucas.costa@platformbuilders.io>
2020-11-21 11:16:27 -0300

    inicial

---
+++ src/main/java/io/builders/discoveryapis/DiscoveryapisApplication.java
@@ -0,0 +1,13 @@
+package io.builders.discoveryapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryapisApplication.class, args);
+	}
+
+}
